970397d6d373e78f2114f9d03896557b848a0e4d
Parth Rathore <42464985+parthrathor@users.noreply.github.com>
2023-07-24 17:31:39 +0530

    Added mongoDB connection and default items list

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+
+//mongoose mongodb database connection
+const url = "mongodb://127.0.0.1:27017/todolistDB";
+mongoose.connect(url).then(function(){
+  console.log("Successfully connected to mongoDB database");
+})
+.catch(function(err){
+  console.log(err);
+})
+
+
+//creating mongoose schema
+const itemSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    required:true
+  }
+});
+
+const Item = new mongoose.model ("Item",itemSchema);
 
 const app = express();
 
@@ -11,15 +29,33 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
+
+//creating todolist default items
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1,item2,item3];
+
+Item.insertMany(defaultItems).then(function(){
+  console.log("Default Items inserted");
+})
+.catch(function(err){
+  console.log(err);
+})
 
 app.get("/", function(req, res) {
-
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
+  res.render("list", {listTitle: Today, newListItems: items});
 });
 
 app.post("/", function(req, res){
@@ -46,3 +82,5 @@ app.get("/about", function(req, res){
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+// mongoose.connection.close()
